2932c1c365e28093f6281fbe3ed7e285e7632df8
Franklin Sahlhoff <franklinsahlhoff@gmail.com>
2021-04-11 18:26:33 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,9 +1,7 @@
- Object.assaign({} , obj , { [artist1]: 'song1'});
- 
- 
- 
- playList = {
+
+ var playList = {
   artist1:'song1',
   artist2:'song2',
   artist3:'song3',
 };
+ Object.assaign({} , obj , { [artist1]: 'song1'});
