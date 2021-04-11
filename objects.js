1548b66d30b34081596d7a37a0215d584dcd0c3c
Franklin Sahlhoff <franklinsahlhoff@gmail.com>
2021-04-11 18:44:35 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -6,7 +6,5 @@ var playlist = {
 function updatePlaylist(playlist,artist1){
   playlist[artist1] = 'song1';
 }
-function removeFromPlaylist(playlist, artist2){
-  return Object.assign({}, obj, {[artist2]:
-  'song2'});
-}
+
+delete playlist.artist1;
